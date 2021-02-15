ada406704a957af4615346d04a546f6047f2fadc
Lala-banners <lala.l.banno@hotmail.com>
2021-02-15 15:28:05 +1100

    Started runnables

--- MobileIntro/Assets/BreadAndButter/Core/RunnableBehaviour.cs
+++ MobileIntro/Assets/BreadAndButter/Core/RunnableBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InvalidOperationException = System.InvalidOperationException;
 
 namespace BreadAndButter
 {
@@ -8,14 +9,27 @@ namespace BreadAndButter
     {
         public bool Enabled { get; set; }
 
+        private bool isSetup = false;
+
         public void Run(params object[] _params)
         {
-            
+            //If the runnable is enabled, run its OnRun function with passed value
+            if (Enabled)
+            {
+                OnRun(_params);
+            }
         }
 
         public void Setup(params object[] _params)
         {
-            
+            if (isSetup)
+            {
+                throw new InvalidOperationException("Runnable already set up");
+            }
+
+            //Run the OnSetup function and flag this as setup
+            OnSetup(_params);
+            isSetup = true;
         }
 
         protected abstract void OnSetup(params object[] _params);
